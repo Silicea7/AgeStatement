4ad5f29b6b7b8c7d5f842f80b3b08b3081ee1371
Paulina <iciekpaulina@gmail.com>
2021-08-08 18:55:03 +0200

    podawanie wieku przez użytkowanika w pętli while

--- src/AgeStatement.java
+++ src/AgeStatement.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 public class AgeStatement {
     public static void main(String[] args) {
-        int humanAge = 0;
+        Scanner inputScanner = new Scanner(System.in);
+        int humanAge =0;
+        while (humanAge >= 0) {
+            System.out.println("enter age");
+            humanAge = inputScanner.nextInt();
 
-        if (humanAge < 0 || humanAge > 120) return;
-        if (humanAge > 80) {
-            System.out.println("człowiek jest stary");
-        } else if (humanAge <= 80 && humanAge > 40) {
-            System.out.println("człowiek jest w średnim wieku");
-        } else if (humanAge <= 40 && humanAge > 15) {
-            System.out.println("człowiek jest młody");
-        } else {
-            System.out.println("człowiek jest w wieku dziecięcym");
+            if (humanAge < 0 || humanAge > 120) return;
+            if (humanAge > 80) {
+                System.out.println("człowiek jest stary");
+            } else if (humanAge <= 80 && humanAge > 40) {
+                System.out.println("człowiek jest w średnim wieku");
+            } else if (humanAge <= 40 && humanAge > 15) {
+                System.out.println("człowiek jest młody");
+            } else {
+                System.out.println("człowiek jest w wieku dziecięcym");
+            }
         }
     }
 }
